dc17521cd1c0054adda18f1dd4c12a80586ef907
RistelMari <157936693+RistelMari@users.noreply.github.com>
2025-04-23 13:18:32 +0800

    Revert "Update RandomQuestionDisplay.cs"
    This reverts commit e253ba3ad66146ad2f64e0f5f9a7b2ba5813a01d.

--- Assets/RandomQuestionDisplay.cs
+++ Assets/RandomQuestionDisplay.cs
@@ -20,7 +20,6 @@ public class RandomQuestionDisplay : MonoBehaviour
     private Question[] currentQuestions;
     private List<int> unusedIndices = new List<int>();
     private int currentQuestionIndex = -1;
-    public int finalScore;
     private int score = 0;
     private int totalQuestions = 0;
     private int questionsAnswered = 0;
@@ -75,11 +74,8 @@ public class RandomQuestionDisplay : MonoBehaviour
             }
 
             if (summaryScoreText != null)
-            {
                 summaryScoreText.text = "Final Score: " + score + "/" + totalQuestions;
-                finalScore = score;
-                Debug.Log("The Final Score is:" + finalScore);
-            }
+
             if (summaryPanel != null)
                 summaryPanel.SetActive(true);
 
